41116ddd32aaa082004ef39089698cd707b0bd3a
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-06-17 18:22:07 -0500

    display modifications

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,26 +1,27 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const article = document.querySelector('input[name="post-article"]').value;
-    const response = await fetch(`/api/posts`, {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        article,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const article = document.querySelector('textarea[name="post-article"]').value;
+  console.log(title, article);
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      article,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
   }
-  
-  document
-    .querySelector(".new-post-form")
-    .addEventListener("submit", newFormHandler);
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
